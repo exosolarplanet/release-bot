a501ff58e077b4ea51a7446898224d069696f6aa
exosolarplanet <ecedenniz@gmail.com>
2023-10-29 15:12:25 +0000

    await response

--- index.js
+++ index.js
@@ -2,43 +2,45 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { Octokit } = require('octokit');
 
-try {
-  const octokit = new Octokit({ 
-    auth: process.env.token,
-  });
+async function createPr() {
+    try {
+    const octokit = new Octokit({ 
+        auth: process.env.token,
+    });
 
-  const imageName = core.getInput('image_name');
-  console.log(`Image name is: ${imageName}`);
+    const imageName = core.getInput('image_name');
+    console.log(`Image name is: ${imageName}`);
 
-  const imageVersion = core.getInput('image_version');
-  console.log(`Image version is: ${imageVersion}`);
+    const imageVersion = core.getInput('image_version');
+    console.log(`Image version is: ${imageVersion}`);
 
-  const artifactoryPath = core.getInput('artifactory_path');
-  console.log(`Artifactory path is: ${artifactoryPath}`);
+    const artifactoryPath = core.getInput('artifactory_path');
+    console.log(`Artifactory path is: ${artifactoryPath}`);
 
-  const payloadJson = github.context.payload;
-  const repoName = payloadJson.repository.name;
+    const payloadJson = github.context.payload;
+    const repoName = payloadJson.repository.name;
 
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-//   console.log(`The event payload: ${payload}`);
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    //   console.log(`The event payload: ${payload}`);
 
-  const response = octokit.request('GET /repos/{owner}/{repo}/git/refs/{ref}', {
-    owner: 'exosolarplanet',
-    repo: repoName,
-    ref: 'heads/main'
-  })
+    const response = octokit.request('GET /repos/{owner}/{repo}/git/refs/{ref}', {
+        owner: 'exosolarplanet',
+        repo: repoName,
+        ref: 'heads/main'
+    }).then()
 
-  console.log(response);
+    console.log((await response).data);
 
-//   octokit.rest.git.createRef({
-//     owner: 'exosolarplanet',
-//     repo: repoName,
-//     ref: 'refs/heads/pr-branch',
+    //   octokit.rest.git.createRef({
+    //     owner: 'exosolarplanet',
+    //     repo: repoName,
+    //     ref: 'refs/heads/pr-branch',
 
 
-//   })
+    //   })
 
-}catch{
-    core.setFailed(error.message);
-    octokit.setFailed(error.message);
+    }catch{
+        core.setFailed(error.message);
+        octokit.setFailed(error.message);
+    }
 }
