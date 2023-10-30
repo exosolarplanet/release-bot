41fb0d364a997d1a29d22b878bca0a1a6b7d9b03
exosolarplanet <ecedenniz@gmail.com>
2023-10-30 20:28:03 +0000

    refactor

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function main() {
     const octokit = new Octokit({ 
         auth: process.env.token,
     });
-    
+
     const user = 'exosolarplanet';
     const branch = 'pr-branch';
 
@@ -78,34 +78,60 @@ async function main() {
     const updatedContent = Buffer.from(YAML.stringify(contentYaml)).toString('base64');
 
     await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
-        owner: 'exosolarplanet',
+        owner: user,
         repo: repoName,
         path: 'helm/Chart.yaml',
         message: 'Update Chart dependencies',
         committer: {
-          name: 'exosolarplanet',
+          name: user,
           email: 'ecedenniz@gmail.com'
         },
         content: updatedContent, // need to convert to base64
         sha: fileSHA,
-        branch: 'pr-branch',
+        branch: branch,
         headers: {
           'X-GitHub-Api-Version': '2022-11-28'
         }
       });
 
-      await octokit.request('POST /repos/{owner}/{repo}/pulls', {
-        owner: 'exosolarplanet',
+    let prNumber = '';
+    await octokit.request('POST /repos/{owner}/{repo}/pulls', {
+        owner: user,
         repo: repoName,
         title: 'Amazing new feature',
         body: 'Please pull these awesome changes in!',
         head: 'exosolarplanet:pr-branch',
         base: 'main',
         headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
+            'X-GitHub-Api-Version': '2022-11-28'
         }
-      })
-    
+        }).then(result => {
+        prNumber = result.data.number;
+        console.log(prNumber);
+    });
+
+    await octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/reviews', {
+        owner: user,
+        repo: repoName,
+        pull_number: prNumber,
+        body: 'This is close to perfect! Please address the suggested inline change.',
+        event: 'APPROVE',
+        headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+        }
+    });
+
+    // await octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/reviews/{review_id}/events', {
+    //     owner: user,
+    //     repo: repoName,
+    //     pull_number: prNumber,
+    //     review_id: 'REVIEW_ID',
+    //     body: 'Here is the body for the review.',
+    //     event: 'REQUEST_CHANGES',
+    //     headers: {
+    //         'X-GitHub-Api-Version': '2022-11-28'
+    //     }
+    // });
 }
 
 main();
